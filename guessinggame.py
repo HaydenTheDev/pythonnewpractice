60668aa62e5aa74b18bd8a936d01287e7926878d
Hayden Bazor <hbazor@murraystate.edu>
2020-06-22 10:39:11 -0500

    guessing game practice with random module and import. challenge solution

--- guessinggame.py
+++ guessinggame.py
@@ -1,17 +1,25 @@
-answer = 5
+import random
 
-print("Please guess a number between 1 and 10: ")
-guess = int(input())
-
-if guess == answer:
-    print("You got it first try")
-else:
-    if guess < answer:
-        print("Please guess higher")
-    else:
-        print("Please guess lower")
+highest = 10
+answer = random.randint(1, highest)
+print(answer)   #  TODO remove after testing
+print("Please guess a number between 1 and {}: ".format(highest))
+guess = 0
+while guess != answer:
     guess = int(input())
+
     if guess == answer:
         print("Well done, you guessed it correctly")
+        break
     else:
-        print("Sorry, you guess incorrectly")
+        if guess < answer:
+            print("Please guess higher")
+        else:
+            print("Please guess lower")
+
+        # guess = int(input())
+        # if guess == answer:
+        #     print("Well done, you guessed it correctly")
+        # else:
+        #     print("Sorry, you guess incorrectly")
+
