d48ed61caf1984b0dfa6f9c7393f167f448cf789
Hayden Bazor <hbazor@murraystate.edu>
2020-06-23 12:22:17 -0500

    understanding iterators. plus the challenge

--- iterators.py
+++ iterators.py
@@ -16,8 +16,16 @@ string = "1234567890"
 # print(next(my_iterator))
 # print(next(my_iterator))
 
-for char in string:
-    print(char)
+# for char in string:
+#     print(char)
+#
+# for char in iter(string):
+#     print(char)
 
-for char in iter(string):
-    print(char)
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+my_iterator = iter(my_list)
+
+for n in range(0, len(my_list)):
+    next_item = (next(my_iterator))
+    print(next_item)
