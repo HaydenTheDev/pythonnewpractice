b68df59e315d536b5587d72a8654ecc21564e3ec
Hayden Bazor <hbazor@murraystate.edu>
2020-06-25 11:36:54 -0500

    keys

--- dictionaries.py
+++ dictionaries.py
@@ -6,3 +6,7 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
 
 print(fruit)
 print(fruit["lemon"])  #this is the key to access description
+fruit["pear"] = "an odd shaped apple" #adding a new value
+print(fruit)
+fruit["pear"] = "great with tequila"
+print(fruit["pear"])
