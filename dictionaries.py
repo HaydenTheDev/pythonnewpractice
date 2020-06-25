d052229f8c875ae3f63c527aa05ae5036f114f74
Hayden Bazor <hbazor@murraystate.edu>
2020-06-25 12:06:19 -0500

    keys, dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,24 +1,36 @@
-fruit = {"orange": "a sweet, orange, citrus fruit",
-        "apple":"good for making cider",
-        "lemon":"a sour yellow citrus fruit",
-        "grape": "a small sweet fruit growing in bunches",
-        "lime": "a sour, green citrus fruit"}
+fruit = {"orange": "a sweet, orange, citrus fruit", "apple": "good for making cider",
+         "lemon": "a sour yellow citrus fruit", "grape": "a small sweet fruit growing in bunches",
+         "lime": "a sour, green citrus fruit", "pear": "great with tequila"}
 
 # print(fruit)
 # print(fruit["lemon"])  #  this is the key to access description
 # fruit["pear"] = "an odd shaped apple" #  adding a new value
 # print(fruit)
-fruit["pear"] = "great with tequila"
 # print(fruit["pear"])
 # del fruit["lemon"] #  deletes key lemon
 # # del fruit #would delete entire dictionary
 # print(fruit)
-while True:
-    dict_key = input("Please enter a fruit")
-    if dict_key == "quit":
-        break
-    if dict_key in fruit:
-        description = fruit.get(dict_key)
-        print(description)
-    else:
-        print("We dont have have {}".format(dict_key))
+# while True:
+#     dict_key = input("Please enter a fruit")
+#     if dict_key == "quit":
+#         break
+#     if dict_key in fruit:
+#         description = fruit.get(dict_key)
+#         print(description)
+#     else:
+#         print("We dont have have {}".format(dict_key))
+#
+# for snack in fruit:
+#     print(fruit[snack])
+
+# WAYS OF CREATING DICTIONARIES THAT LOOK GOOD.
+
+# ordered_key = list(fruit.keys())
+# ordered_key.sort()
+
+# ordered_keys = sorted(list(fruit.keys()))
+# for f in ordered_keys:
+#     print(f + " - " + fruit[f])
+
+for f in sorted(fruit.keys()):
+    print(f + " - " + fruit[f])
