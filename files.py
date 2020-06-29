50bb51a70bbf373e37cb9a066ba38ef7e07bac24
Hayden Bazor <hbazor@murraystate.edu>
2020-06-29 13:36:39 -0500

    input and output of a file, different variations

--- files.py
+++ files.py
@@ -1,9 +1,9 @@
-# jabber = open("/Users/power/Desktop/original.txt", 'r')
-# for line in jabber:
-#     if "did" in line.lower():
-#         print(line, end='')
-#
-# jabber.close()
+jabber = open("C:\\Users\\power\\Desktop\\original.txt", 'r')
+for line in jabber:
+    if "did" in line.lower():
+        print(line, end='')
+
+jabber.close()
 
 # with open("original.txt", 'r') as jabber:
 #     for line in jabber:
@@ -16,9 +16,16 @@
 #         print(line, end='')
 #         line = jabber.readline()
 
-with open("original.txt" 'r') as jabber:
-    lines = jabber.readlines()
-print(lines)
+# with open("original.txt", 'r') as jabber:
+#     lines = jabber.readlines()
+# print(lines)
+#
+# for line in lines:
+#     print(line, end='')
 
-for line in lines:
-    print(line, end='')
+# with open("original.txt", 'r') as jabber:
+#     lines = jabber.readlines()
+# print(lines)
+#
+# for line in lines[::-1]:
+#     print(line, end='')
