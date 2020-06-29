7b8b54668a10e7594a9069d42cb887e5c03cc9df
Hayden Bazor <hbazor@murraystate.edu>
2020-06-29 15:28:08 -0500

    webbrowser module

--- browser.py
+++ browser.py
@@ -1,6 +1,9 @@
 import webbrowser
 
+# webbrowser.open("https://www.Python.org")
 
-for i in range(10):
-    print(1, 2, 3, 4, 5, 6, 7, 8, 9, sep=': ', end=' ')
+# help(webbrowser)
+# for i in range(10):
+#     print(1, 2, 3, 4, 5, 6, 7, 8, 9, sep=': ', end=' ')
+
 
