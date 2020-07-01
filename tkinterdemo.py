197d5356f95f2f2f5dfbd0a0df2218578af99533
Hayden Bazor <hbazor@murraystate.edu>
2020-07-01 16:56:49 -0500

    tkinter buttons and dialogue window

--- tkinterdemo.py
+++ tkinterdemo.py
@@ -11,5 +11,12 @@ label = tkinter.Label(mainWindow, text="Hello World")
 label.pack(side='top')
 
 canvas = tkinter.Canvas(mainWindow, relief='raised', borderwidth=1)
-canvas.pack(side='left', fill=tkinter.Y)
+canvas.pack(side='left')
+
+button1 = tkinter.Button(mainWindow, text="Button1")
+button2 = tkinter.Button(mainWindow, text="Button2")
+button3 = tkinter.Button(mainWindow, text="Button3")
+button1.pack(side='left', anchor='n')
+button2.pack(side='left', anchor='s')
+button3.pack(side='left', anchor='e')
 mainWindow.mainloop()
