9f7263bfcf3c449368497206cc7d772e3750bf47
Hayden Bazor <hbazor@murraystate.edu>
2020-07-01 16:51:19 -0500

    tkinter

--- tkinterdemo.py
+++ tkinterdemo.py
@@ -3,12 +3,13 @@ try:
 except ImportError:
     import tkinter as tkinter
 
-print(tkinter.TkVersion)
-print(tkinter.TclVersion)
-
-
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Hello how are you")
 mainWindow.geometry("640x400")
+label = tkinter.Label(mainWindow, text="Hello World")
+label.pack(side='top')
+
+canvas = tkinter.Canvas(mainWindow, relief='raised', borderwidth=1)
+canvas.pack(side='left', fill=tkinter.Y)
 mainWindow.mainloop()
