12d151a5d5bde47651ce1ed44f6a7b47001a8c5b
Hayden Bazor <hbazor@murraystate.edu>
2020-07-01 15:13:00 -0500

    pytz and timezones aware times with local and UTC

--- awaretime.py
+++ awaretime.py
@@ -10,5 +10,14 @@ print("Naive UTC {}".format(utc_time))
 aware_local_time = pytz.utc.localize(local_time)
 aware_utc_time = pytz.utc.localize(utc_time)
 
-print("Aware local time{}, time ".format(aware_local_time))
-print("Aware local time{}, time ".format(aware_utc_time))
+print("Aware local time {}, time zone {}".format(aware_local_time, aware_local_time.tzinfo))
+print("Aware local time {}, time time zone {}".format(aware_utc_time, aware_utc_time.tzinfo))
+
+gap_time = datetime.datetime(2020, 10, 25, 1, 30, 0, 0)
+print(gap_time)
+print(gap_time.timestamp())
+
+s = 1445733000
+t = s + (60 + 60)
+
+gb = pytz.timezone('GB')
