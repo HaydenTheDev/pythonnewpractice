7a2833fbdf260a62c313fd890e9601aaa366a969
Hayden Bazor <hbazor@murraystate.edu>
2020-07-01 17:59:04 -0500

    tkinter GUI example.

--- tkinterdemo.py
+++ tkinterdemo.py
@@ -25,7 +25,17 @@ button3 = tkinter.Button(rightFrame, text="Button3")
 button1.grid(row=0, column=0)
 button2.grid(row=1, column=0)
 button3.grid(row=2, column=0)
+
+# configuring the columns
 mainWindow.columnconfigure(0, weight=1)
 mainWindow.columnconfigure(1, weight=1)
 mainWindow.grid_columnconfigure(2, weight=1)
+
+leftFrame.config(relief='sunken', borderwidth=1)
+rightFrame.config(relief='sunken', borderwidth=1)
+leftFrame.grid(sticky='ns')
+rightFrame.grid(sticky='new')
+
+rightFrame.columnconfigure(0, weight=1)
+button2.grid(sticky='ew')
 mainWindow.mainloop()
