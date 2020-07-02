2739052409e8974056456edeff1e48cd2e766ae0
Hayden Bazor <hbazor@murraystate.edu>
2020-07-02 15:01:09 -0500

    understanding functions, arguments

--- functions.py
+++ functions.py
@@ -1,5 +1,17 @@
 def python_food():
+    width = 80
     print("pizza and fries")
 
+print(python_food())
 
-python_food()
+
+
+def center_text(text):
+    left_margin = (80 - len(text)) // 2
+    print(" " * left_margin, text)
+
+center_text("pizza is amazing")
+
+
+
+
