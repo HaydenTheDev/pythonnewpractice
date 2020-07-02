fa940d1fdcc177e108d0f28c461684f3fdc6e8c5
Hayden Bazor <hbazor@murraystate.edu>
2020-07-02 16:57:26 -0500

    understanding functions, arguments

--- functions.py
+++ functions.py
@@ -1,17 +1,23 @@
-def python_food():
-    width = 80
-    print("pizza and fries")
+# def python_food():
+#     width = 80
+#     print("pizza and fries")
+#
+#
+# print(python_food())
+#
+#
+# def center_text(text):
+#     text = str(text)
+#     left_margin = (80 - len(text)) // 2
+#     print(" " * left_margin, text)
+# center_text("pizza is amazing")
 
-print(python_food())
 
-
-
-def center_text(text):
+def some_text(*args):
+    text = ""
+    for arg in args:
+        text += str(arg) + ""
     left_margin = (80 - len(text)) // 2
     print(" " * left_margin, text)
 
-center_text("pizza is amazing")
-
-
-
-
+some_text("first", "Second", 3, 4, "Pizza")
