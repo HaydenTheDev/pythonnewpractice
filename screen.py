8112c40d8932ea615d274d05cbe49a6c886db4ec
Hayden Bazor <hbazor@murraystate.edu>
2020-07-02 11:30:23 -0500

    tkinter GUI example. scroll bar and files, dates and spinners

--- screen.py
+++ screen.py
@@ -38,6 +38,7 @@ optionFrame.grid(row=1, column=2, sticky='ne')
 
 rbValue = tkinter.IntVar()
 rbValue.set(3)
+
 # add radio buttons
 radio1 = tkinter.Radiobutton(optionFrame, text="Filename", value=1, variable=rbValue)
 radio2 = tkinter.Radiobutton(optionFrame, text="Path", value=2, variable=rbValue)
@@ -46,6 +47,47 @@ radio1.grid(row=0, column=0, sticky='w')
 radio2.grid(row=1, column=0, sticky='w')
 radio3.grid(row=2, column=0, sticky='w')
 
+# Widget to display the result.
+resultLabel = tkinter.Label(mainWindow, text="Result")
+resultLabel.grid(row=2, column=2, sticky='nw')
+result = tkinter.Entry(mainWindow)
+result.grid(row=2, column=2, sticky='sw')
+
+# frame
+timeFrame = tkinter.LabelFrame(mainWindow, text="Time")
+timeFrame.grid(row=3, column=0, sticky='new')
+
+#Time spinners
+hourSpinner = tkinter.Spinbox(timeFrame, width=2, values=tuple(range(0, 24)))
+minuteSpinner = tkinter.Spinbox(timeFrame, width=2,values=tuple(range(0, 60)))
+secondSpinner = tkinter.Spinbox(timeFrame, width=2, values=tuple(range(0, 60)))
+hourSpinner.grid(row=0, column=0)
+tkinter.Label(timeFrame, text=': ').grid(row=0, column=1)
+minuteSpinner.grid(row=0, column=2)
+tkinter.Label(timeFrame, text=': ').grid(row=0, column=3)
+secondSpinner.grid(row=0, column=4)
+timeFrame['padx'] = 36
+
+# frame for the date spinners
+dateFrame = tkinter.Frame(mainWindow)
+dateFrame.grid(row=4,column=0, sticky='new')
+
+# datelabels
+dayLabel = tkinter.Label(dateFrame, text="Day")
+monthLabel = tkinter.Label(dateFrame, text="Month")
+yearLabel = tkinter.Label(dateFrame, text="Year")
+dayLabel.grid(row=0, column=0, sticky='w')
+monthLabel.grid(row=0, column=1, sticky='w')
+yearLabel.grid(row=0, column=2, sticky='w')
+
+# Date spinners
+daySpin = tkinter.Spinbox(dateFrame, width=5, values=tuple(range(1, 31)))
+monthSpin = tkinter.Spinbox(dateFrame, width=5, values=("Jan", "Feb", "March", "April", "May", "June", "July", "August",
+                                                        "September", "October", "November", "December"))
+yearSpin = tkinter.Spinbox(dateFrame, width=5, values=tuple(range(2000, 2099)))
+daySpin.grid(row=1, column=0)
+monthSpin.grid(row=1, column=1)
+yearSpin.grid(row=1, column=2)
 mainWindow.mainloop()
 
 print(rbValue.get())
