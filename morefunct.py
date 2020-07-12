5e551a7c805266af9553b02f5da53898bd7e93f2
Hayden Bazor <hbazor@murraystate.edu>
2020-07-12 16:27:42 -0500

    more on functions and tkinter, graph-parabola, clean up the code and fix functions and errors.

--- morefunct.py
+++ morefunct.py
@@ -4,40 +4,39 @@ except ImportError:
     import tkinter as tkinter
 
 
-def parabola(x):
-    y = x * x / 100
-    return y
+def parabola(page, size):
+    for x in range(-size, size):
+        y = x * x / size
+        plot(page, x, y)
 
 
-def draw_axes(canvas):
-    canvas.update()
-    x_origin = canvas.winfo_width() / 2
-    y_origin = canvas.winfo_height() / 2
-    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
-    canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
+def draw_axes(page):
+    page.update()
+    x_origin = page.winfo_width() / 2
+    y_origin = page.winfo_height() / 2
+    page.configure(scrollregion=(-x_origin, -y_origin,
+                                 x_origin, y_origin))
+    page.create_line(-x_origin, 0,
+                     x_origin, 0, fill="white")
+    page.create_line(0, y_origin, 0,
+                     -y_origin, fill="white")
+    print(locals())
 
 
-def plot(canvas, x, y):
-    canvas.create_line(x, y, x + 1, y + 1, fill="red")
+def plot(page, x, y):
+    page.create_line(x, -y, x + 1, -y + 1, fill="purple")
 
 
 mainWindow = tkinter.Tk()
 
-
 mainWindow.title("Parabola")
 mainWindow.geometry("640x480")
 
-canvas = tkinter.Canvas(mainWindow, width=320, height=480)
+canvas = tkinter.Canvas(mainWindow, width=640, height=480, background="black")
 canvas.grid(row=0, column=0)
 
-canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background="blue")
-canvas2.grid(row=0, column=1)
 draw_axes(canvas)
-draw_axes(canvas2)
 
-for x in range(-100, 100):
-    y = parabola(x)
-    plot(canvas, x, -y)
-
+parabola(canvas, 100)
+parabola(canvas, 200)
 mainWindow.mainloop()
