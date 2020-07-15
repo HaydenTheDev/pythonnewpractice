184ddf1190abe24bb432703b5e7aed6cd50d0117
Hayden Bazor <hbazor@murraystate.edu>
2020-07-15 14:42:21 -0500

    namespaces and more information on scopes and recursion

--- scope.py
+++ scope.py
@@ -4,8 +4,29 @@ def fact(n):
     if n > 1:
         for f in range(2, n + 1):
             result *= f
-        return result
+    return result
 
 
 for i in range(130):
-    print(1, fact(i))
+    print(i, fact(i))
+
+
+def factorial(n):
+    # n! can also be defined as n * (n-1)!
+    """ calculate n! recursively"""
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+def fib(n):
+    """F(n) = F(n - 1) + F(n - 2)"""
+    if n < 2:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+for i in range(130):
+    print(i, fib(i))
