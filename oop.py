cd09b600503ccd49212fa6e84ac050472c6f9593
Hayden Bazor <hbazor@murraystate.edu>
2020-07-15 16:34:42 -0500

    Object Orientated Programming

--- oop.py
+++ oop.py
@@ -15,3 +15,20 @@ kenwood.price = 12.75
 print(kenwood.price)
 
 hamilton = Kettle("Hamilton", 14.55)
+
+print("Models: {} = {}, {} = {}".format(kenwood.make,
+                                        kenwood.price,
+                                        hamilton.make,
+                                        hamilton.price))
+
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+
+
+"""
+Class: template for creating objects. All objects created using the same class will have the same
+characteristics.
+Object: an instance of a class.
+Instantiate: Create an instance of a class.
+Method: A function defined in a class.
+Attribute: A variable bound to an instance of class.
+"""
