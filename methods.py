742a0f9f3aa7f7b2318bd6b7339692d699bc378c
Hayden Bazor <hbazor@murraystate.edu>
2020-07-17 17:34:44 -0500

    Methods

--- methods.py
+++ methods.py
@@ -9,10 +9,14 @@ class Account:
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
+            self.show_balance()
 
     def withdraw(self, amount):
-        if amount > 0:
+        if 0 < amount <= self.balance:
             self.balance -= amount
+        else:
+            print("You don't have that much to withdraw. ")
+        self.show_balance()
 
     def show_balance(self):
         print("Balance is {} ".format(self.balance))
@@ -23,6 +27,5 @@ if __name__ == "__main__":
     hayden.show_balance()
 
     hayden.deposit(1000)
-    hayden.show_balance()
     hayden.withdraw(500)
-    hayden.show_balance()
+    hayden.withdraw(4000)
