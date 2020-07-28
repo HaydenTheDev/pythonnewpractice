ca58ed8ac141f05c16cbcd64b4f7e94e56c88832
Hayden Bazor <hbazor@murraystate.edu>
2020-07-27 20:54:53 -0500

    DocString and raw literals

---
+++ song.py
@@ -0,0 +1,21 @@
+class Song:
+    """Class to present a song
+    
+    Attributes
+        title (str): The title of the song
+        artist (Artist): ana artist object representing the songs creator.
+        duration (int): The duration of the song in seconds. May be zero
+    """
+
+    def __init__(self, title, artist, duration):
+        """Song init method
+
+        args:
+            title (str): Initalises the 'title' attribute
+            artist (Artist): At artist object representing the song's creator
+            duration (Optional(int): Initial value for the 'duration' attribute
+                Will default to zero if not specified
+        """
+        self.title = title
+        self.artist = artist
+        self.duration = duration
