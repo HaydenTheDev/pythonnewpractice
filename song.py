d3ca5342b94701190657c2ceba7bf66553c5f9af
Hayden Bazor <hbazor@murraystate.edu>
2020-07-27 21:32:40 -0500

    added album class with functions

--- song.py
+++ song.py
@@ -13,11 +13,12 @@ class Song:
         self.artist = artist
         self.duration = duration
 
+
 class Album:
     """ Class to represent an Album, using it's track list
 
     Attributes:
-        album_name (str): The name of the album.
+        name (str): The name of the album.
         year (int): The year the album was released.
         artist (Artist): The artist responsible for the album
         if not specified, the artist will default to the artist with the name
@@ -32,7 +33,7 @@ class Album:
         self.name = name
         self.year = year
         if artist is None:
-            self.artist = artist("Various Artists")
+            self.artist = Artist("Various Artists")
         else:
             self.artist = artist
 
@@ -51,3 +52,29 @@ class Album:
             self.tracks.append(song)
         else:
             self.tracks.insert(positions, song)
+
+
+class Artist:
+    """Basic class to store artist details.
+    
+    Attributes:
+        name (str): The name of the artist.
+        albums (List(Album)): A list of the albums by this artist.
+            The list includes only albums in this collection, it is
+            not an exhaustive list of the artist's publishes albums.
+
+    Methods:
+            add_album: Use to add a new alubm to the artist's albums list"""
+
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album):
+        """Add a new album to the list.
+
+        Args:
+            album (Album): Album to add to the list.
+                if the album is already present, it will not be added again.
+        """
+        self.albums.append(album)
